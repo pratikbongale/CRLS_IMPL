43f46fb2dbb904fb1b937704fb1d0ba833b99800
pratikbongale <psb4346@rit.edu>
2019-06-12 19:12:59 -0400

    DirecctAddressing tested, works fine

--- S3_Data_Structures/CH11_HashTables/DirectAddressing.py
+++ S3_Data_Structures/CH11_HashTables/DirectAddressing.py
@@ -7,6 +7,9 @@ class Employee:
         self.name = name
         self.age = age
 
+    def __str__(self):
+        return '[Key: {}, Name: {}, Age: {}]'.format(self.key, self.name, self.age)
+
 class DirectAddressingHT(HashTable):
 
     def __init__(self, table_size):
@@ -17,24 +20,58 @@ class DirectAddressingHT(HashTable):
         # return the object if found, else return None
 
         if not self.is_valid_key(k):
+            print('Not a valid key')
+            return None
+
+        elif not self.ht[k]:
+            print('Object not found')
             return None
 
         return self.ht[k]
 
     def insert(self, x):
         if not self.is_valid_key(x.key):
-            print('Cannot store in hash table, out of bounds')
+            print('Not a valid key')
 
         self.ht[x.key] = x
 
     def delete(self, x):
 
         if not self.is_valid_key(x.key):
+            print('Not a valid key')
+            return None
+
+        elif not self.ht[x.key]:
             print('Object not found')
             return None
 
+        z = self.ht[x.key]
         self.ht[x.key] = None
+
+        return z
 
     def is_valid_key(self, k):
         return k < self.size
+
+if __name__ == '__main__':
+
+    x = Employee(1, 'abc', 30)
+    y = Employee(4, 'pqr', 25)
+    ht = DirectAddressingHT(10)
+    z = ht.search(15)             # => invalid key
+    print(z) if z else ''           # => ''
+
+    ht.insert(x)                   # =>
+    z = ht.search(1)
+    print(z) if z else ''        # => (1, 'abc', 30)
+
+    z = ht.delete(y)        # => not found
+    print(z) if z else ''   # => ''
+
+    ht.insert(y)
+    z = ht.delete(y)        # => found
+    print(z) if z else ''  # => (4, 'pqr', 25)
+
+    ht.search(4)        # => not found
+
 
